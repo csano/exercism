a7dc8c7c48cb7c2bfd81834b5256ca04167a4f78
Chris Sano <cmsano@gmail.com>
2016-12-15 16:54:55 -0800

    Solution for SumOfMultiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+public class SumOfMultiples
+{
+    public static int To(int[] numbers, int max)
+    {
+        var output = 0;
+        for (var i = 0; i < max; i++)
+        {
+            if (numbers.Any(num => i % num == 0))
+            {
+                output += i;
+            }
+        }
+        return output;
+    }
+}

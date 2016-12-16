1c7b1216b6b27962a1fb9e8600fa36dc54840591
Chris Sano <cmsano@gmail.com>
2016-12-15 21:20:14 -0800

    Solution for C# grains problem

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Grains
+{
+    public static ulong Total()
+    {
+        ulong sum = 0;
+        for(var i = 1; i <= 64; i++)
+        {
+            sum += Square(i);
+        }
+        return sum;
+    }
+
+    public static ulong Square(int i)
+    {
+        return (ulong) Math.Pow(2, i-1);
+    }
+}
---
+++ csharp/grains/GrainsTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class GrainsTest
+{
+    [Test]
+    public void Test_square_1()
+    {
+        Assert.That(Grains.Square(1), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Test_square_2()
+    {
+        Assert.That(Grains.Square(2), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Test_square_3()
+    {
+        Assert.That(Grains.Square(3), Is.EqualTo(4));
+    }
+
+    [Test]
+    public void Test_square_4()
+    {
+        Assert.That(Grains.Square(4), Is.EqualTo(8));
+    }
+
+    [Test]
+    public void Test_square_16()
+    {
+        Assert.That(Grains.Square(16), Is.EqualTo(32768));
+    }
+
+    [Test]
+    public void Test_square_32()
+    {
+        Assert.That(Grains.Square(32), Is.EqualTo(2147483648));
+    }
+
+    [Test]
+    public void Test_square_64()
+    {
+        Assert.That(Grains.Square(64), Is.EqualTo(9223372036854775808));
+    }
+
+    [Test]
+    public void Test_total_grains()
+    {
+        Assert.That(Grains.Total(), Is.EqualTo(18446744073709551615));
+    }
+}
+
+public class Grains
+{
+    public static ulong Total()
+    {
+        ulong sum = 0;
+        for(var i = 1; i <= 64; i++)
+        {
+            sum += Square(i);
+        }
+        return sum;
+    }
+
+    public static ulong Square(int i)
+    {
+        return (ulong) Math.Pow(2, i-1);
+    }
+}

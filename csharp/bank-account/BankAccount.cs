5fe3e6218e13de3087dfec8a09190d93ef92f850
Chris Sano <cmsano@gmail.com>
2016-12-15 22:57:53 -0800

    Solution for C# bank account problem

---
+++ csharp/bank-account/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class BankAccount
+{
+    private bool isOpen;
+    private int balance;
+    private readonly object locker = new object();
+
+    public void Open()
+    {
+        isOpen = true;
+    }
+
+    public void UpdateBalance(int amount)
+    {
+        lock(locker)
+        {
+            balance += amount;
+        }
+    }
+
+    public int GetBalance()
+    {
+        if (!isOpen)
+        {
+            throw new InvalidOperationException();
+        }
+        return balance;
+    }
+
+    public void Close()
+    {
+        isOpen = false;
+    }
+}

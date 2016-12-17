b7309c4ab42c965336b21d1740c71183952a88fc
Chris Sano <chris@ideabolt.io>
2016-12-16 22:14:17 -0800

    Cleaned up year solution

--- swift/leap/year.swift
+++ swift/leap/year.swift
@@ -1,17 +1,15 @@
 
 class Year {
-    var calendarYear : Int32;
-    init(calendarYear : Int32) {
+    
+    let calendarYear : Int;
+    
+    init(calendarYear : Int) {
         self.calendarYear = calendarYear;
     }
+    
     var isLeapYear : Bool {
         get {
-            if (calendarYear % 4 == 0) {
-                if (calendarYear % 100 != 0 || calendarYear % 400 == 0) {
-                    return true;
-                }
-            }
-            return false;
+            return (calendarYear % 4 == 0) && (calendarYear % 100 != 0 || calendarYear % 400 == 0);
         }
     }
 }

5c23e0d21079a1ead8244687b2f8e04f49e9326c
Chris Sano <chris@ideabolt.io>
2016-12-18 00:04:41 -0800

    Solution for Swift sumOfMultiples problem

---
+++ swift/sum-of-multiples/sumOfMultiples.swift
@@ -0,0 +1,15 @@
+
+class SumOfMultiples {
+    static func toLimit(_ limit : Int, inMultiples: Array<Int>) -> Int {
+        var sum = 0;
+        for index in 1..<limit {
+            for factor in inMultiples {
+                if factor != 0 && index % factor == 0 {
+                    sum += index
+                    break
+                }
+            }
+        }
+        return sum
+    }
+}

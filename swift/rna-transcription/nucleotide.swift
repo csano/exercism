82f6354b20afeb408b9e16e27675af85f091049a
Chris Sano <chris@ideabolt.io>
2016-12-18 00:38:04 -0800

    Cleaned up code

--- swift/rna-transcription/nucleotide.swift
+++ swift/rna-transcription/nucleotide.swift
@@ -3,9 +3,7 @@ class Nucleotide {
     let mapping: [Character : Character] = [ "G" : "C", "C" : "G", "T" : "A", "A" : "U" ]
     let strand : String
     var complementOfDNA : String {
-        get {
-            return String(strand.characters.map({ mapping[$0]! }))
-        }
+        return String(strand.characters.map({ mapping[$0]! }))
     }
     init(_ strand : String) {
         self.strand = strand;

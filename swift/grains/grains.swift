c6ab21ac30552579c537105f69efe7cd3c9629d8
Chris Sano <chris@ideabolt.io>
2016-12-20 11:25:33 -0700

    Solution for Swift grains problem

---
+++ swift/grains/grains.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+class Grains {
+    static func createErrorMessage(_ input : Int) -> String {
+        return "Input[\(input)] invalid. Input should be between 1 and 64 (inclusive)"
+    }
+    static func square(_ squares : Int) throws -> UInt {
+        guard squares < 1 else {
+            throw GrainsError.inputTooLow(message: createErrorMessage(squares))
+        }
+        guard squares > 64 else {
+            throw GrainsError.inputTooHigh(message: createErrorMessage(squares))
+        }
+        return grainsOnSquare(squares)
+    }
+    static func grainsOnSquare(_ squares : Int) -> UInt{
+        return UInt(pow(2, Double(squares - 1)))
+
+    }
+    static var total : UInt {
+        return (1...64).map(grainsOnSquare).reduce(0, +)
+    }
+    enum GrainsError : Error {
+        case inputTooLow(message : String)
+        case inputTooHigh(message : String)
+    }
+}

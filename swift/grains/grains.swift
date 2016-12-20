3e00047e75d16994f118778ab1f1a0009b4a4ed5
Chris Sano <chris@ideabolt.io>
2016-12-20 11:39:51 -0700

    Cleaned up grains exercise

--- swift/grains/grains.swift
+++ swift/grains/grains.swift
@@ -5,15 +5,15 @@ class Grains {
         return "Input[\(input)] invalid. Input should be between 1 and 64 (inclusive)"
     }
     static func square(_ squares : Int) throws -> UInt {
-        guard squares < 1 else {
+        guard squares > 0 else {
             throw GrainsError.inputTooLow(message: createErrorMessage(squares))
         }
-        guard squares > 64 else {
+        guard squares < 65 else {
             throw GrainsError.inputTooHigh(message: createErrorMessage(squares))
         }
         return grainsOnSquare(squares)
     }
-    static func grainsOnSquare(_ squares : Int) -> UInt{
+    static func grainsOnSquare(_ squares : Int) -> UInt {
         return UInt(pow(2, Double(squares - 1)))
 
     }

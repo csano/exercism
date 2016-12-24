86f64f39e87b77138c9cb6d6f5d0c4032221b72a
Chris Sano <chris@ideabolt.io>
2016-12-23 23:18:34 -0700

    Solution for Elixir word-count problem

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,15 @@
+defmodule Words do
+  defp string_to_word_list(str) do
+    str |> String.downcase |> String.split(~r/(_|[^\w\p{Pd}])+/u, trim: true)
+  end
+
+  defp add_to_dict(x, acc) do
+    Dict.update(acc, x, 1, &(&1 + 1))
+  end
+
+  def count(sentence) do
+    string_to_word_list(sentence) |> Enum.reduce(%{}, &add_to_dict/2)
+  end
+end
+
+IO.inspect Words.count("test test foo bar")

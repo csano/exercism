257c9788fb5bad91c57385c8a451e04ea68eec7b
Chris Sano <cmsano@gmail.com>
2016-12-26 22:13:12 -0700

    Update to anagram solution

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,15 +1,17 @@
 defmodule Anagram do
   defp sort_characters_in_word(word) do
     word
-    |> String.downcase
     |> String.graphemes
     |> Enum.sort
     |> Enum.join
   end
 
   defp is_anagram?(word1, word2) do
-    String.downcase(word1) !== String.downcase(word2) &&
-    sort_characters_in_word(word1) === sort_characters_in_word(word2) 
+    lower_word1 = String.downcase(word1) 
+    lower_word2 = String.downcase(word2) 
+    lower_word1 != lower_word2 &&
+
+    sort_characters_in_word(lower_word1) === sort_characters_in_word(lower_word2) 
   end
 
   def match(base, candidates) do

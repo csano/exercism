b6844a82b1797c33619622743c2b1eb08205535e
Chris Sano <cmsano@gmail.com>
2017-01-04 13:30:45 -0800

    Solution for Proverb C# problem

--- csharp/scrabble-score/ScrabbleScoreTest.cs
+++ csharp/scrabble-score/ScrabbleScoreTest.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
@@ -53,43 +51,5 @@ public class ScrabbleScoreTest
     public void Scores_are_case_insensitive()
     {
         Assert.That(Scrabble.Score("OXYPHENBUTAZONE"), Is.EqualTo(41));
-    }
-}
-
-public class Scrabble
-{
-    private static readonly Dictionary<string, int> ScoreDictionary = new Dictionary<string, int>
-    {
-        {"A", 1},
-        {"E", 1},
-        {"I", 1},
-        {"O", 1},
-        {"U", 1},
-        {"L", 1},
-        {"N", 1},
-        {"R", 1},
-        {"S", 1},
-        {"T", 1},
-        {"D", 2},
-        {"G", 2},
-        {"B", 3},
-        {"C", 3},
-        {"M", 3},
-        {"P", 3},
-        {"F", 4},
-        {"H", 4},
-        {"V", 4},
-        {"W", 4},
-        {"Y", 4},
-        {"K", 5},
-        {"J", 8},
-        {"X", 8},
-        {"Q", 10},
-        {"Z", 10}
-    };
-
-    public static int Score(string word)
-    {
-        return string.IsNullOrEmpty(word?.Trim()) || word.StartsWith(" ") ? 0 : word.ToCharArray().Sum(letter => ScoreDictionary[letter.ToString().ToUpper()]);
     }
 }

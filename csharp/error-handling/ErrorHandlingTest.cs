0320c6f3bbc8218277181c756ab7f55d1de0c2dd
Chris Sano <cmsano@gmail.com>
2017-01-17 12:31:56 -0800

    Uncommitted changes

--- csharp/error-handling/ErrorHandlingTest.cs
+++ csharp/error-handling/ErrorHandlingTest.cs
@@ -62,36 +62,3 @@ public class ErrorHandlingTest
         Assert.That(disposableResouce.IsDisposed, Is.EqualTo(true));
     }
 }
-
-public class ErrorHandling
-{
-    public static void HandleErrorByThrowingException()
-    {
-        throw new Exception();
-    }
-
-    public static int? HandleErrorByReturningNullableType<T>(T parameter)
-    {
-        int output;
-        if (int.TryParse(parameter.ToString(), out output))
-        {
-            return output;
-        }
-        return null;
-    }
-
-    public static bool HandleErrorWithOutParam(string parameter, out int result)
-    {
-        var output = HandleErrorByReturningNullableType(parameter);
-        result = output ?? 0;
-        return output != null;
-    }
-
-    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(ErrorHandlingTest.DisposableResource disposbaleResource)
-    {
-        using (disposbaleResource)
-        {
-            throw new Exception();
-        }
-    }
-}

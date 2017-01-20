dacaf6848cc145cafd8c7c32b153c1ab5a326472
Chris Sano <cmsano@gmail.com>
2017-01-20 10:10:16 -0800

    Quick optimization

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -3,7 +3,7 @@
     public static NumberType Classify(int number)
     {
         var sum = 0;
-        for (var i = 1; i < number; i++)
+        for (var i = 1; i <= number / 2; i++)
         {
             if (number % i == 0)
             {

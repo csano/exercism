8e2527dc87dd7445041928f5a4e5950f8434013c
Chris Sano <cmsano@gmail.com>
2017-02-17 23:08:44 -0800

    Removed extraneous code

--- swift/isogram/Isogram.swift
+++ swift/isogram/Isogram.swift
@@ -3,6 +3,6 @@ import Foundation
 class Isogram {
     static func isIsogram(_ input : String) -> Bool {
         var filtered = input.lowercased().characters.filter({ CharacterSet.lowercaseLetters.contains(UnicodeScalar(String($0))!)})
-        return Array(Set(filtered)).count == filtered.count
+        return Set(filtered).count == filtered.count
     }
 }

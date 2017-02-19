f4a23039bf8adae55a0dfc5fb243c34fb728040b
Chris Sano <cmsano@gmail.com>
2017-02-18 22:59:38 -0800

    Updated Robot solution to prevent duplicate names

--- swift/robot-name/RobotName.swift
+++ swift/robot-name/RobotName.swift
@@ -2,32 +2,41 @@ import Foundation
 
 class Robot {
     var name : String = ""
+    static var usedNames : [String] = []
 
     init() {
         resetName()
     }
 
     func resetName() {
-        name = setName()
+        name = Robot.setName()
     }
 
-    func generateNumbers() -> String {
+    static func generateNumbers() -> String {
         return String(format: "%3d", (randomize(999) + 1))
     }
 
-    func generateLetters() -> String {
+    static func generateLetters() -> String {
         return String("\(getRandomLetter())\(getRandomLetter())")
     }
 
-    func getRandomLetter() -> String {
+    static func getRandomLetter() -> String {
        return String(describing: UnicodeScalar(randomize(26) + 65)!)
     }
 
-    func randomize(_ upperBound: UInt32) -> UInt32 {
+    static func randomize(_ upperBound: UInt32) -> UInt32 {
         return arc4random_uniform(upperBound)
     }
 
-    func setName() -> String {
-        return "\(generateLetters())\(generateNumbers())"
+    static func buildString() -> String {
+       return "\(generateLetters())\(generateNumbers())"
+    }
+
+    static func setName() -> String {
+        var newName : String
+        repeat {
+            newName = buildString()
+        } while usedNames.contains(where: { $0 == newName })
+        return newName
     }
 }

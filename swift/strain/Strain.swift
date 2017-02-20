82ae3c7af7977cf4ac5289b667c0df16016b5a7f
Chris Sano <cmsano@gmail.com>
2017-02-19 22:26:19 -0800

    Cleaned up code

--- swift/strain/Strain.swift
+++ swift/strain/Strain.swift
@@ -1,19 +1,17 @@
 
 extension Sequence {
     func keep(_ predicate: (Iterator.Element) -> Bool) -> [Iterator.Element] {
+        return filter({ predicate($0) })
+    }
+
+    func discard(_ predicate: (Iterator.Element) -> Bool) -> [Iterator.Element] {
+        return filter({ !predicate($0) })
+    }
+
+    func filter(_ predicate: (Iterator.Element) -> Bool) -> [Iterator.Element] {
         var out : [Iterator.Element] = []
         for element in self {
             if predicate(element) {
-                out.append(element)
-            }
-        }
-        return out
-    }
-    
-    func discard(_ predicate: (Iterator.Element) -> Bool) -> [Iterator.Element] {
-        var out : [Iterator.Element] = []
-        for element in self {
-            if !predicate(element) {
                 out.append(element)
             }
         }

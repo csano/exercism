52991e2f575ba5f5e3e10f52466dfa9290054edd
Chris Sano <cmsano@gmail.com>
2017-02-20 00:03:21 -0800

    Updated Sieve problem

--- swift/sieve/Sieve.swift
+++ swift/sieve/Sieve.swift
@@ -12,13 +12,11 @@ class Sieve {
         for i in 2..<last  {
             if !map.contains(where: { $0.key == i }) {
                 map[i] = true
-                var j = i * 2
-                while j < last {
+                for j in ((i + 1)..<last).filter({ $0 % i == 0 }) {
                     map[j] = false
-                    j += i
                 }
             }
         }
-        return map.filter({ $0.value! == true }).map({ $0.key }).sorted(by: { $0 < $1 })
+        return map.filter({ $0.value! }).map({ $0.key }).sorted(by: { $0 < $1 })
     }
 }

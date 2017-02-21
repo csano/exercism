82fba5c205906e47ccc1507132c1c8dce5587d53
Chris Sano <cmsano@gmail.com>
2017-02-21 15:06:20 -0800

    Initial implementation of attack C# problem

--- csharp/queen-attack/QueenAttackTest.cs
+++ csharp/queen-attack/QueenAttackTest.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Xml.Schema;
 using NUnit.Framework;
 
 public class QueenAttackTest
@@ -11,7 +15,6 @@ public class QueenAttackTest
         Assert.Throws<ArgumentException>(() => new Queens(white, black));
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Cannot_attack()
     {
@@ -19,7 +22,6 @@ public class QueenAttackTest
         Assert.False(queens.CanAttack());
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Can_attack_on_same_row()
     {
@@ -27,7 +29,6 @@ public class QueenAttackTest
         Assert.True(queens.CanAttack());
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Can_attack_on_same_column()
     {
@@ -35,7 +36,6 @@ public class QueenAttackTest
         Assert.True(queens.CanAttack());
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Can_attack_on_diagonal()
     {
@@ -43,7 +43,6 @@ public class QueenAttackTest
         Assert.True(queens.CanAttack());
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Can_attack_on_other_diagonal()
     {
@@ -51,7 +50,6 @@ public class QueenAttackTest
         Assert.True(queens.CanAttack());
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Can_attack_on_yet_another_diagonal()
     {
@@ -59,11 +57,44 @@ public class QueenAttackTest
         Assert.True(queens.CanAttack());
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Can_attack_on_a_diagonal_slanted_the_other_way()
     {
         var queens = new Queens(new Queen(6, 1), new Queen(1, 6));
         Assert.True(queens.CanAttack());
+    }
+}
+
+public class Queens
+{
+    private readonly Queen white;
+    private readonly Queen black;
+
+    public Queens(Queen white, Queen black)
+    {
+        this.white = white;
+        this.black = black;
+    }
+
+    public bool CanAttack()
+    {
+        var wQueen = new[] {white.X, white.Y};
+        var bQueen = new[] {black.X, black.Y};
+
+        var o = wQueen.Zip(bQueen, (i1, i2) => new Tuple<int, int>(i1, i2)).Select(x => Math.Abs(x.Item1 - x.Item2)).ToList();
+
+        return o.All(x => x == o.First()) || o.Any(x => x == 0);
+    }
+}
+
+public class Queen
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Queen(int x, int y)
+    {
+        X = x;
+        Y = y;
     }
 }

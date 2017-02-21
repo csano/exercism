cd9d3395ff5e848cb4bc7a1afa8f9d92f7ec1f04
Chris Sano <cmsano@gmail.com>
2017-02-21 10:00:14 -0800

    Moved class to its own file

--- csharp/beer-song/BeerTest.cs
+++ csharp/beer-song/BeerTest.cs
@@ -2,7 +2,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 public class BeerTests
 {
@@ -20,66 +19,5 @@ public class BeerTests
     public string Sing(int start, int stop)
     {
         return Beer.Sing(start, stop);
-    }
-}
-
-public static class StringExtensions
-{
-    public static string FirstToUpper(this string s)
-    {
-        if (string.IsNullOrEmpty(s))
-        {
-            return string.Empty;
-        }
-        return char.ToUpper(s[0]) + s.Substring(1);
-    }
-}
-
-public class Beer
-{
-    public static string Sing(int start, int stop)
-    {
-        var verses = new StringBuilder();
-        for (var i = start; i >= stop; i--)
-        {
-            verses.Append(Verse(i) + "\n");
-        }
-        return verses.ToString();
-    }
-
-    public static string AddPlural(int count)
-    {
-        return count != 1 ? "s" : "";
-    }
-
-    public static string Verse(int number)
-    {
-        var nextNumber = number == 0 ? 99 : number - 1;
-        return $"{GenerateBottleCountString(number).FirstToUpper()} bottle{AddPlural(number)} of beer on the wall, {GenerateBottleCountString(number)} bottle{AddPlural(number)} of beer.\n{GenerateFinalSentence(number)}, {GenerateBottleCountString(nextNumber)} bottle{AddPlural(nextNumber)} of beer on the wall.\n";
-    }
-
-    private static string GenerateFinalSentence(int number)
-    {
-        return number == 0 ? GoToTheStoreAndBuyMore() : TakeBottleDownAndPassItAround(number);
-    }
-
-    private static string TakeBottleDownAndPassItAround(int number)
-    {
-        return $"Take {GenerateItOrOneString(number)} down and pass it around";
-    }
-
-    private static string GenerateItOrOneString(int number)
-    {
-        return number == 1 ? "it" : "one";
-    }
-
-    private static string GenerateBottleCountString(int i)
-    {
-        return i == 0 ? "no more" : i.ToString();
-    }
-
-    private static string GoToTheStoreAndBuyMore()
-    {
-        return "Go to the store and buy some more";
     }
 }

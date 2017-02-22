462a1c30cd873b8d652d69f32adbdd60cb415944
Chris Sano <cmsano@gmail.com>
2017-02-22 11:45:01 -0800

    Submitting file

---
+++ csharp/queen-attack/Queens.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+
+public class Queens
+{
+    private readonly Queen white;
+    private readonly Queen black;
+
+    public Queens(Queen white, Queen black)
+    {
+        this.white = white;
+        this.black = black;
+    }
+
+    public bool CanAttack()
+    {
+        var whiteQueenCoordinates = new[] { white.X, white.Y };
+        var blackQueenCoordinates = new[] { black.X, black.Y };
+
+        var list = whiteQueenCoordinates
+                        .Zip(blackQueenCoordinates, (i1, i2) => new Tuple<int, int>(i1, i2))
+                        .Select(x => Math.Abs(x.Item1 - x.Item2))
+                        .ToList();
+
+        return list.All(x => x == list.First()) || list.Any(x => x == 0);
+    }
+}
+
+public class Queen
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Queen(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}

179b8f27efaa7e7bfbcf3e8e7ef01a001e4cea24
Chris Sano <cmsano@gmail.com>
2017-02-22 21:56:08 -0800

    Cleaned up solution

--- swift/perfect-numbers/PerfectNumbersTests.swift
+++ swift/perfect-numbers/PerfectNumbersTests.swift
@@ -1,30 +1,5 @@
 import XCTest
 
-enum Classification {
-    case abundant
-    case deficient
-    case perfect
-}
-
-class NumberClassifier {
-    let number : Int
-
-    init(number number: Int) {
-        self.number = number
-    }
-
-    var classification : Classification {
-        var sum = (1..<number).filter({ number % $0 == 0}).reduce(0, +)
-
-        if sum == number {
-            return .perfect
-        } else if sum > number {
-            return .abundant
-        }
-        return .deficient
-    }
-}
-
 class PerfectNumbersTests: XCTestCase {
     func testPerfect() {
         let numberClassifier = NumberClassifier(number: 6)
@@ -39,7 +14,6 @@ class PerfectNumbersTests: XCTestCase {
     func testDeficient() {
         let numberClassifier = NumberClassifier(number: 13)
         XCTAssertEqual([.deficient], [numberClassifier.classification])
-
     }
 
     func testAbundant() {

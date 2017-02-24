e8b199b8e268b7fe8c537a5072bde2c6bf5f68c6
Chris Sano <cmsano@gmail.com>
2017-02-24 09:24:10 -0800

    Solution for secret handshake C# problem

---
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class SecretHandshake
+{
+    public static string[] Commands(int numberOfCommands)
+    {
+        var mappings = new Dictionary<int, string>()
+        {
+            {0x1, "wink"},
+            {0x2, "double blink"},
+            {0x4, "close your eyes"},
+            {0x8, "jump"},
+        };
+
+        var handshake = mappings.Where(x => (x.Key & numberOfCommands) != 0).Select(x => x.Value);
+
+        return ((numberOfCommands & 10000) != 0 ? handshake.Reverse() : handshake).ToArray();
+    }
+}

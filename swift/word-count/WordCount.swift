173a582a2e6b926bee3139ac692be5bf89196f49
Chris Sano <cmsano@gmail.com>
2017-02-26 13:52:26 -0800

    Solution for word-count Swift problem

---
+++ swift/word-count/WordCount.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+extension Sequence {
+    func groupBy<T: Hashable>(key: (Iterator.Element) -> T) -> [T: Int] {
+        var output : [T: Int] = [:]
+        for element in self {
+            let key = key(element)
+            if case nil = output[key] {
+                output[key] = 1
+            } else {
+                output[key]! += 1
+            }
+        }
+        return output
+    }
+}
+
+extension String {
+    func stripCharacters(_ characters: CharacterSet) -> String {
+        return String(self.characters.filter({characters.contains(String($0).unicodeScalars.first!)}))
+    }
+}
+
+extension CharacterSet {
+    static var LettersNumbersAndWhitespace : CharacterSet {
+        return CharacterSet.letters.union(CharacterSet.decimalDigits).union(CharacterSet(charactersIn:" "))
+    }
+}
+
+class WordCount {
+    let words : String
+    init(words words : String) {
+        self.words = words
+    }
+    func count() -> [String:Int] {
+        return self.words.stripCharacters(CharacterSet.LettersNumbersAndWhitespace).components(separatedBy:CharacterSet.whitespaces).filter({ !$0.isEmpty }).groupBy(key: { $0.lowercased() })
+    }
+}
+

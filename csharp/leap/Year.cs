88816962b9027f1dc9f0403a743826c060d32f20
Chris Sano <cmsano@gmail.com>
2017-03-21 09:23:17 -0700

    Simplified

--- csharp/leap/Year.cs
+++ csharp/leap/Year.cs
@@ -1,10 +1,6 @@
 public class Year {
-  public static bool IsLeap(int year) {
-    if (year % 4 == 0) {
-      if (year % 100 != 0 || year % 400 == 0) {
-        return true;
-      }
-    }
-    return false;
+  public static bool IsLeap(int year)
+  {
+      return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
   }
 }

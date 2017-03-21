d3f2ce3252e963f9c377fcfad3ef0b4ae0b906e4
Chris Sano <cmsano@gmail.com>
2017-03-21 09:39:20 -0700

    Simplified logic

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -4,11 +4,6 @@ public class Pangram
 {
     public static bool IsPangram(string sequence)
     {
-        var checklist = new bool[26];
-        foreach (var c in sequence.ToLower().Where(x => x >= 'a' && x <= 'z'))
-        {
-            checklist[c - 'a'] = true;
-        }
-        return !checklist.Any(x => !x);
+        return sequence.ToLower().Where(x => x >= 'a' && x <= 'z').GroupBy(x => x).Count() == 26;
     }
 }
